4ccd7694ebe8e0aabd1e0363f4f0be997ca5c357
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2025-09-02 13:34:39 +0300

    - Made a general view of the program - Added MainWindow, and 2 dialog windows WorkOne and WorkTwo

---
+++ Laboratory/Laboratory/Dialog_Windows/WorkTwo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Laboratory
+{
+    public partial class WorkTwo : Window
+    {
+        public WorkTwo()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
---
+++ Laboratory/Laboratory/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratory
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (WorkComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string selectedValue = selectedItem.Content.ToString();
+
+                switch (selectedValue)
+                {
+                    case "Work-1":
+                        WorkOne workOne = new WorkOne();
+                        workOne.Show();
+                        break;
+                    case "Work-2":
+                        WorkTwo workTwo = new WorkTwo();
+                        workTwo.Show();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
